10fe9771622d99ef23168d9a87bc8927c4b8e992
Hrangett <orangett217@gmail.com>
2022-07-01 17:06:44 +0900

    20220701 commit

---
+++ Socket/zombie.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t pid = fork();
+
+	if(pid == 0)
+	{
+		puts("Hi, I am a child prosess");
+	}
+	else
+	{ 
+		printf("Child Process ID : %d \n", pid);
+		sleep(30);	//Sleep 30 sec.
+	}
+
+	if( pid == 0)
+		puts("End child process");
+	else
+		puts("End parent process");
+	
+	
+	return 0;
+}
