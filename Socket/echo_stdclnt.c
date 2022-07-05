cb9843155672caf32c4665d8dba26ebdfa8ca9cd
Hrangett <orangett217@gmail.com>
2022-07-05 17:26:52 +0900

    20220705 _ epoll, Level trigger, Edge Triggerr...etc

--- Socket/echo_stdclnt.c
+++ Socket/echo_stdclnt.c
@@ -4,23 +4,23 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-
 #define BUF_SIZE 1024
 void error_handling(char *message);
 
-int main(int argc, char *argv[])
+int main(int argc, char*argv[])
 {
 	int sock;
 	char message[BUF_SIZE];
 	int str_len;
 	struct sockaddr_in serv_adr;
+	FILE * readfp;
+	FILE * writefp;
 
-	if(argc != 3)
+	if(argc!=3)
 	{
-		printf("Usage : %s <IP> <port>\n", argv[0]);
+		printf("Usage : %s <IP><port>\n", argv[0]);
 		exit(1);
 	}
-
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	if(sock == -1)
 		error_handling("socket() error");
@@ -31,31 +31,35 @@ int main(int argc, char *argv[])
 	serv_adr.sin_port = htons(atoi(argv[2]));
 
 	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
-		error_handling("connect() error");
-	else
-		puts("Connected.........");
+		error_handling("connect error");
 
+	else
+		puts("Connected...");
+
+	readfp=fdopen(sock,"r");
+	writefp = fdopen(sock,"w");
 	while(1)
 	{
-		fputs("Input message(Q to quit): ", stdout);
-		fgets(message, BUF_SIZE, stdin);
-
-		if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+		fputs("Input message(Q to quit):", stdout);
+		fgets(message, BUF_SIZE,stdin);
+		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
 			break;
 
-		write(sock, message, strlen(message));
-		str_len = read(sock, message, BUF_SIZE-1);
-		message[str_len] = 0;
-		printf("Messsage from server : %s", message);
-	}
+		fputs(message, writefp);
+		fflush(writefp);
+		fgets(message, BUF_SIZE, readfp);
+		printf("Message from server: %s",message);
 
-	close(sock);
+	}
+	fclose(writefp);
+	fclose(readfp);
 	return 0;
 }
 
+
 void error_handling(char *message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+     fputs(message,stderr);
+     fputc('\n',stderr);
+     exit(1);
 }
--- Socket/file_server.c
+++ Socket/file_server.c
@@ -21,17 +21,17 @@ int main(int argc, char*argv[])
 
 	if(argc != 2)
 	{
-		printf("Usage : %s <port?\n",argv[0]);
+		printf("Usage : %s <port>\n",argv[0]);
 		exit(1);
 	}
 
-	fp = fopen("a.dat","rv");
+	fp = fopen(argv[1],"rb");
 	serv_sd = socket(PF_INET,SOCK_STREAM,0);
 
 	memset(&serv_adr,0,sizeof(serv_adr));
 	serv_adr.sin_family = AF_INET;
 	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_adr.sin_port = htons(atoi(argv[1]));
+	serv_adr.sin_port = htons(atoi("9090"));
 
 
 	bind(serv_sd, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
